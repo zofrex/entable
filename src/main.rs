88c82243be6b37480e585b2ee13bc3c7de8b0bc0
Thom Wiggers <thom@thomwiggers.nl>
2018-09-03 11:39:03 +0200

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
 extern crate regex;
 
 use regex::Regex;
-use std::iter;
 use std::cmp;
 use std::io;
 use std::io::Read;
+use std::iter;
 
 fn main() {
     let divider_re = Regex::new(r"^\+[\-\+]*\+$").unwrap();
 
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input).expect("Something went wrong reading from stdin, somehow");
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Something went wrong reading from stdin, somehow");
 
     let lines: Vec<&str> = input.split_terminator("\n").collect();
-    
+
     let mut counts: Vec<usize> = vec![];
 
     // First pass: determine column widths sufficient to contain all data
     for line in lines.clone() {
-        let split_char = if divider_re.is_match(line) {
-            "+"
-        }
-        else {
-            "|"
-        };
+        let split_char = if divider_re.is_match(line) { "+" } else { "|" };
 
         let this_line_counts = line.split(split_char).map(|s| s.len());
-        counts = this_line_counts.zip(counts.into_iter().chain(iter::repeat(0))).map(|(previous_max, current_size)| cmp::max(previous_max, current_size)).collect();
+        counts = this_line_counts
+            .zip(counts.into_iter().chain(iter::repeat(0)))
+            .map(|(previous_max, current_size)| cmp::max(previous_max, current_size))
+            .collect();
     }
 
     // Second pass: print out tabulated columns
@@ -42,8 +42,7 @@ fn main() {
                 }
             }
             println!("+");
-        }
-        else {
+        } else {
             let data = line.split("|");
             for (i, d) in data.enumerate() {
                 if d.len() == 0 {
@@ -53,5 +52,5 @@ fn main() {
             }
             println!("|");
         }
-    }  
+    }
 }
